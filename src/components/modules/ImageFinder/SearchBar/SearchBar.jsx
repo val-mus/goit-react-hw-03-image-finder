b1702c972685fd0981b86d7fd52dd45b5d66298a
Valentyn Musiienko <musiienkovalentyn@gmail.com>
2023-02-07 22:55:54 +0200

    upd

--- src/components/modules/ImageFinder/SearchBar/SearchBar.jsx
+++ src/components/modules/ImageFinder/SearchBar/SearchBar.jsx
@@ -1,17 +1,59 @@
-import SearchBarForm from './SearchBarForm/SearchBarForm';
-import styles from './search-bar.module.css';
 import PropTypes from 'prop-types';
+import { Component } from 'react';
 
-const SearchBar = ({onSubmit}) => {
-  return (
-    <header className={styles.SearchBar}>
-      <SearchBarForm onSubmit={onSubmit} />
-    </header>
-  );
-};
+import s from './search-bar.module.css';
 
+class SearchBar extends Component {
+  state = {
+    search: '',
+  };
 
-SearchBarForm.propTypes = {
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    onSubmit({ ...this.state });
+    this.reset();
+  };
+
+  reset() {
+    this.setState({
+      search: '',
+    });
+  }
+
+  render() {
+    const { search } = this.state;
+    const { handleChange, handleSubmit } = this;
+    return (
+      <header className={s.SearchBar}>
+      <form className={s.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={s.SearchForm__button}>
+          <span className={s.SearchForm__button_label}>Search</span>
+        </button>
+
+        <input
+          className={s.SearchForm__input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={search}
+          onChange={handleChange}
+          name="search"
+          required
+        />
+      </form>
+      </header>
+    );
+  }
+}
+
+SearchBar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
 export default SearchBar;
